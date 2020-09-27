462f9bb8a4d8b8d417b0b63874e19c4d03d58ad0
junwusuoxi <2967791299@qq.com>
2020-09-27 22:46:17 +0800

    使用黄油刀

--- app/src/main/java/com/example/todayinformation/MainActivity.java
+++ app/src/main/java/com/example/todayinformation/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.todayinformation;
 
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+@ViewInject(mainlayoutid = R.layout.activity_main)
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fac_main)
+    FloatingActionButton facMain;
+    @BindView(R.id.rb_main_shanghai)
+    RadioButton rbMainShanghai;
+    @BindView(R.id.rb_main_hangzhou)
+    RadioButton rbMainHangzhou;
+    @BindView(R.id.rg_main_top)
+    RadioGroup rgMainTop;
+    @BindView(R.id.fl_main_bottom)
+    FrameLayout flMainBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    @OnClick(R.id.fac_main)
+    public void onClick() {
+        return;
     }
 }
---
+++ app/src/main/java/com/example/todayinformation/ViewInject.java
@@ -0,0 +1,14 @@
+package com.example.todayinformation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME) //运行时注解
+@Target(TYPE)   //类 接口 注解
+
+public @interface ViewInject {
+    int mainlayoutid() default -1;
+}
