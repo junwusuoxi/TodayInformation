220f5b7fe04dde0fbee9e9eb76cf7732ea1c4e83
junwusuoxi <2967791299@qq.com>
2020-10-03 16:25:49 +0800

    创建菜单栏动画

--- app/src/main/java/com/example/todayinformation/MainActivity.java
+++ app/src/main/java/com/example/todayinformation/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.todayinformation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 @ViewInject(mainlayoutid = R.layout.activity_main)
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.fac_main)
-    FloatingActionButton facMain;
+
     @BindView(R.id.rb_main_shanghai)
     RadioButton rbMainShanghai;
     @BindView(R.id.rb_main_hangzhou)
@@ -27,15 +26,54 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup rgMainTop;
     @BindView(R.id.fl_main_bottom)
     FrameLayout flMainBottom;
+    @BindView(R.id.rb_main_beijing)
+    RadioButton rbMainBeijing;
+    @BindView(R.id.rb_main_shenzhen)
+    RadioButton rbMainShenzhen;
+    @BindView(R.id.rg_main_bottom)
+    RadioGroup rgMainBottom;
+    @BindView(R.id.fac_main_home)
+    FloatingActionButton facMainHome;
+
+    private boolean isChangeTopOrButtom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        changAnima(rgMainBottom, rgMainTop);
 
     }
 
-    @OnClick(R.id.fac_main)
-    public void onClick() {
-        return;
+    @OnClick(R.id.fac_main_home)
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.fac_main_home:
+                isChangeTopOrButtom = !isChangeTopOrButtom;
+
+                if (isChangeTopOrButtom) {
+                    changAnima(rgMainTop, rgMainBottom);
+                } else {
+                    changAnima(rgMainBottom, rgMainTop);
+                }
+
+                break;
+        }
+
+    }
+
+    private void changAnima(RadioGroup gone, RadioGroup show) {
+
+//        消失的动画
+        gone.clearAnimation(); //清除自身动画
+        Animation animationGone = AnimationUtils.loadAnimation(this, R.anim.main_tab_translate_hide);
+        gone.startAnimation(animationGone);
+        gone.setVisibility(View.GONE);
+
+//       展示动画
+        show.clearAnimation();
+        Animation animationShow = AnimationUtils.loadAnimation(this, R.anim.main_tab_translate_show);
+        show.startAnimation(animationShow);
+        show.setVisibility(View.VISIBLE);
     }
 }
